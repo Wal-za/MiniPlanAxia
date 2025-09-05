ccb3e72b2ba07f087d752a063035348e629b1094
Waltza1 <111610017+Waltza1@users.noreply.github.com>
2025-09-05 13:48:29 -0500

    finalizado ultimo

--- miniplanaxia/src/components/FormWizard.jsx
+++ miniplanaxia/src/components/FormWizard.jsx
@@ -76,29 +76,20 @@ const nextSection = (sectionData) => {
     const blob = new Blob([response.data], { type: 'application/pdf' });
     const url = URL.createObjectURL(blob);
 
-    // Intentar abrir el PDF en una nueva ventana
     const ventanaPdf = window.open(url, '_blank');
 
-    // Verificar si la ventana emergente se abrió correctamente
     if (ventanaPdf) {
-        // Si se abrió correctamente, realizar las acciones
         localStorage.removeItem('formularioData');
         localStorage.removeItem('formularioStep');
         localStorage.removeItem('wizardData');
         localStorage.removeItem('wizardStep');
 
-        // Redirigir la ventana principal después de 3 segundos
         setTimeout(function() {
-            // Redirigir en la ventana principal para evitar que se cierre la ventana emergente
-            window.location.replace('https://axia.com.co/');  // Usar replace para evitar que el historial se registre
-        }, 3000);  // Espera 3 segundos antes de redirigir
+            window.location.replace('https://axia.com.co/'); 
+        }, 3000);  
     } else {
-        // Si la ventana emergente no se pudo abrir (por ejemplo, está bloqueada)
         alert('No se pudo abrir el PDF. Por favor, habilita las ventanas emergentes en tu navegador.');
-
-        // NO hacer nada, no redirigir, y no cerrar el PDF
-        // Aquí puedes manejar otras acciones si es necesario
-    }
+  }
 })
     .catch(error => {
       console.error('❌ Error al enviar el formulario:', error);
